e4f65de1dba6b01f5ade54d7e31d94255f34f12e
temo927 <temorexviashvili17@gmail.com>
2023-04-02 07:37:38 -0700

    menu fade animation

--- script.js
+++ script.js
@@ -72,3 +72,21 @@ tabsContainer.addEventListener(`click`, function (e) {
       .classList.add(`operations__content--active`);
   }
 });
+// menu fade animation
+const nav = document.querySelector(`.nav`);
+
+const fade = function (e) {
+  if (e.target.classList.contains(`nav__link`)) {
+    const link = e.target;
+    const siblings = link.closest(`.nav`).querySelectorAll(`.nav__link`);
+    const logo = link.closest(`.nav`).querySelector(`img`);
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+// passing argument to handler function
+nav.addEventListener(`mouseover`, fade.bind(0, 5));
+nav.addEventListener(`mouseout`, fade.bind(1));
